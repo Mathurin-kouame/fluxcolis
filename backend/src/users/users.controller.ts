9f3237b9a280db8ec59516de43908aafa1f18608
MACBOOK AIR <macbookair@192.168.1.4>
2026-05-12 22:52:25 +0000

    feat(auth): ajout RolesGuard et sécurisation des routes par rôle

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -2,12 +2,15 @@ import { Controller, Get, Param, Request, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import type { RequestWithUser } from 'src/auth/types/request-user.interface';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   @Get()
   findAll() {
     return this.usersService.findAll();
