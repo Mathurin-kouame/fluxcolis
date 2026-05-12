238ba15a1ec19b610197cccb0ca86d71ae173e46
MACBOOK AIR <macbookair@192.168.1.4>
2026-05-12 21:10:13 +0000

    feat(auth): ajout de req.user et typage sécurisé JWT

---
+++ backend/src/auth/types/jwt-user.type.ts
@@ -0,0 +1,6 @@
+export type JwtUser = {
+  userId: string;
+  name: string;
+  email: string;
+  role: string;
+};
--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Request, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import type { RequestWithUser } from 'src/auth/types/request-user.interface';
 
 @Controller('users')
 export class UsersController {
@@ -16,5 +17,12 @@ export class UsersController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getMe(@Request() req: RequestWithUser) {
+    const user = req.user;
+    return user;
   }
 }
