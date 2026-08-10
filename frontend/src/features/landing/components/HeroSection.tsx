1b17de5998de143db8bdc254e0c2bd4633646eb7
MACBOOK AIR <macbookair@192.168.1.2>
2026-08-10 16:18:58 +0000

    refactor(frontend): update hero and tracking search components

--- frontend/src/features/landing/components/HeroSection.tsx
+++ frontend/src/features/landing/components/HeroSection.tsx
@@ -18,8 +18,8 @@ export const HeroSection = () => {
 
             {/* Badge */}
             <div
-              className="inline-flex items-center gap-2 px-4 py-2 rounded-full bg-blue-100 text-blue-700 text-sm font-medium mb-6">
-              <span className="w-2 h-2 rounded-full bg-blue-600"></span>
+              className="inline-flex items-center gap-2 px-4 py-2 rounded-full bg-blue-100 text-blue-700 text-sm font-medium mb-6 animate-pulse">
+              <span className="w-2 h-2 rounded-full bg-blue-600 animate-bounce"></span>
               plateforme logistique moderne
             </div>
 
--- frontend/src/features/public-tracking/components/TrackingSearch.tsx
+++ frontend/src/features/public-tracking/components/TrackingSearch.tsx
@@ -1,3 +1,5 @@
+import { ChevronLeft } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 interface TrackingPros {
     value: string;
@@ -5,15 +7,23 @@ interface TrackingPros {
     onSearch: () => void;
 }
 
-export const TrackingSearch = ({value, onChange, onSearch}:TrackingPros) => {
+export const TrackingSearch = ({ value, onChange, onSearch }: TrackingPros) => {
+    const navigate = useNavigate()
     return (
         <div className="rounded-t-md m-0.5 bg-linear-to-r from-blue-700 to-indigo-700 p-10 sm:p-8 md:p-8 shadow-sm">
+            <button
+                onClick={() => navigate("/")}
+                className="group mb-4 text-slate-300 hover:text-white cursor-pointer"
+            >
+                <ChevronLeft size={24} className="translate-x-2 transition-all duration-300 group-hover:opacity-100 group-hover:translate-x-0" />
+            </button>
+
             <h2 className="text-center text-2xl md:text-3xl sm:text-4xl font-bold text-white">
-                Suivi de votre colis 
+                Suivi de votre colis
             </h2>
 
             <p className="mt-2 text-center text-sm sm:text-base text-blue-100 max-w-2xl mx-auto">
-               Entrez votre numéro de suivi pour suivre l'état de votre colis en temps réel.
+                Entrez votre numéro de suivi pour suivre l'état de votre colis en temps réel.
             </p>
             <div className="mt-8 flex justify-center flex-col sm:flex-row gap-3 sm:gap-0">
                 <input
